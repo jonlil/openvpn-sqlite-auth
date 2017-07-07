4fbaf8b4a9a6d0cb09ded286d816288c957102f0
Jonas Liljestrand <jonas.liljestrand@hemnet.se>
2017-07-07 09:11:30 +0200

    Default empty values for yubico

--- config.py
+++ config.py
@@ -7,3 +7,7 @@ PASSWORD_LENGTH_MIN = 8
 # Hash algorithm to use for passwords storage. Can be one of:
 # md5, sha1, sha224, sha256, sha384, sha512
 HASH_ALGORITHM = 'sha256'
+
+YUBICO_CLIENT_ID = None
+
+YUBICO_SECRET = None
